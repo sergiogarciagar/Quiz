d46adc30bec85e2032c2aad55b5e43d56187326a
sergiogarciagar <sergiovillacas@gmail.com>
2015-05-03 23:02:54 +0200

    Version con Autoload

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,13 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
-
+// Pagina de creditos
 router.get('/author', function(req, res) {
   res.render('author');
 });
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizId
 //Definicion de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
