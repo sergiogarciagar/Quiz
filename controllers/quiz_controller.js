8e4cc71dde81a0c8c8c8ca34136b10935bdaf5fe
sergiogarciagar <sergiovillacas@gmail.com>
2015-05-04 00:03:44 +0200

    Version con creacion de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -20,6 +20,24 @@ exports.index = function(req, res) {
 	})
 };
 
+//GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build ( // crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+	res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+
+	//guardar en DB los campos pregunta y respuesta de quiz 
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	}) // Redirección HTTP (URL relativo) lista de preguntas
+};
+
 // GET /quizes/:id
 exports.show = function(req, res) {
 	models.Quiz.find(req.params.quizId).then(function(quiz){
