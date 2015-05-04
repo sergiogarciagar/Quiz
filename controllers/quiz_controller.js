682695790ee9a5fdd9ba5439aa722dad869d8629
sergiogarciagar <sergiovillacas@gmail.com>
2015-05-04 11:22:18 +0200

    Version con validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,7 +16,7 @@ exports.load = function(req, res, next, quizId) {
 //GET /quizes
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(function(quizes){
-	res.render('quizes/index.ejs', { quizes: quizes});
+	res.render('quizes/index.ejs', { quizes: quizes, errors: []});
 	})
 };
 
@@ -25,33 +25,46 @@ exports.new = function(req, res) {
 	var quiz = models.Quiz.build ( // crea objeto quiz
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 		);
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
 	var quiz = models.Quiz.build( req.body.quiz );
 
-	//guardar en DB los campos pregunta y respuesta de quiz 
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	}) // Redirección HTTP (URL relativo) lista de preguntas
+	quiz
+	.validate()
+	.then(
+	 function(err) {
+	 	if(err) {
+	 		res.render('quizes/new', {quiz: quiz, errors: err.errors});
+	 	} else {
+	     quiz  //guardar en DB los campos pregunta y respuesta de quiz 
+	     .save({fields: ["pregunta", "respuesta"]})
+	     .then(function(){res.redirect('/quizes')})
+	} // Redirección HTTP (URL relativo) lista de preguntas
+   }
+  ); 
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors: []});
 	})
 };
 
 //GET /quizes/:id/answer
 exports.answer = function(req, res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-	if (req.query.respuesta === req.quiz.respuesta) {
-		res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Correcto'});
-	} else{
-		res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Incorrecto'});
+	var resultado = 'Incorrecto';
+	if(req.query.respuesta === req.quiz.respuesta) {
+		resultado='Correcto';
 	}
-  })
+	res.render(
+		'quizes/answer',
+		{quiz: req.quiz,
+			respuesta: resultado,
+			errors: []
+	}
+  );
 };
